84d25593d624faaa6e1c0d9a8045b6c75daf8f09
LazMan8 <laza.andriamampianina67@outlook.com>
2024-10-31 11:37:46 +0100

    je continue ca pendant le week-end

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,15 +2,64 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
 
-    
+    #[Route(path: '/register', name: 'app_register')]
+    public function register(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        // Si la requête est de type POST, nous traitons les données du formulaire
+        if ($request->isMethod('POST')) {
+            // Récupération des données du formulaire
+            $firstName = $request->request->get('first_name');
+            $lastName = $request->request->get('last_name');
+            $email = $request->request->get('email');
+            $password = $request->request->get('password');
+            $passwordConfirmation = $request->request->get('password_confirmation');
+            $csrfToken = $request->request->get('_csrf_token');
+
+            // Validation CSRF
+            if (!$this->isCsrfTokenValid('authenticate', $csrfToken)) {
+                $this->addFlash('error', 'Token CSRF invalide.');
+                return $this->redirectToRoute('app_register');
+            }
+
+            // Vérification de la confirmation du mot de passe
+            if ($password !== $passwordConfirmation) {
+                $this->addFlash('error', 'Les mots de passe ne correspondent pas.');
+                return $this->redirectToRoute('app_register');
+            }
+
+            // Création de l'entité User
+            $user = new User();
+            $user->setFirstName($firstName);
+            $user->setLastName($lastName);
+            $user->setEmail($email);
+            
+            // Hachage du mot de passe
+            $hashedPassword = $passwordHasher->hashPassword($user, $password);
+            $user->setPassword($hashedPassword);
+
+            // Sauvegarde de l'utilisateur dans la base de données
+            $userRepository->createOneUser($user);
+
+            // Redirection vers la page de connexion ou d'accueil
+            $this->addFlash('success', 'Inscription réussie ! Vous pouvez maintenant vous connecter.');
+            return $this->redirectToRoute('app_login');
+        }
+
+        // Affichage du formulaire d'inscription
+        return $this->render('security/register.html.twig');
+    }
 
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
@@ -24,7 +73,7 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);// , ['last_username' => $lastUsername, 'error' => $error]
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,7 +4,9 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
@@ -14,9 +16,14 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
  */
 class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
-    public function __construct(ManagerRegistry $registry)
+
+    private EntityManagerInterface $em;
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(ManagerRegistry $registry, EntityManagerInterface $em)
     {
         parent::__construct($registry, User::class);
+        $this->em = $em;
     }
 
     /**
@@ -33,6 +40,27 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->getEntityManager()->flush();
     }
 
+    public function createOneUser(User $user)
+    {
+        $conn = $this->em->getConnection();
+
+        // Hachage du mot de passe avant l'insertion
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $user->getPassword());
+
+        // Préparation de la requête d'insertion
+        $reqStmt = $conn->prepare("INSERT INTO user (first_name, last_name, email, password)
+                                   VALUES
+                                   (:first_name, :last_name, :email, :password)");
+
+        // Liaison des valeurs des champs aux paramètres SQL
+        $reqStmt->bindValue(':first_name', $user->getFirstName(), \PDO::PARAM_STR);
+        $reqStmt->bindValue(':last_name', $user->getLastName(), \PDO::PARAM_STR);
+        $reqStmt->bindValue(':email', $user->getEmail(), \PDO::PARAM_STR);
+        $reqStmt->bindValue(':password', $hashedPassword, \PDO::PARAM_STR);
+
+        // Exécution de la requête
+        $reqStmt->execute();
+    }
     //    /**
     //     * @return User[] Returns an array of User objects
     //     */
